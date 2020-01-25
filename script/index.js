773f3d09a716c7a257a3fc25bf6043890d287941
Nvmeless <alexandre.quilan.delaistre@gmail.com>
2020-01-25 19:12:05 +0100

    Pointer Pos X/y

--- script/index.js
+++ script/index.js
@@ -1,1 +1,12 @@
-alert("hello world");
+window.onload = init;
+function init() {
+    if (window.Event) {
+        document.captureEvents(Event.MOUSEMOVE);
+    }
+    document.onmousemove = getCursorXY;
+}
+
+function getCursorXY(e) {
+    let posX = (window.Event) ? e.pageX : event.clientX + (document.documentElement.scrollLeft ? document.documentElement.scrollLeft : document.body.scrollLeft);
+    let posY = (window.Event) ? e.pageY : event.clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop);
+}
